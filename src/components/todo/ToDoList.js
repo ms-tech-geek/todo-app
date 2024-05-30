9d2145027c25e4ec3a25173a25ed6353f5ad8195
Mayank Sethi <mayank@digibrain.co.in>
2024-05-30 14:13:00 +0530

    refactor todolist filteration

--- src/components/todo/ToDoList.js
+++ src/components/todo/ToDoList.js
@@ -1,5 +1,35 @@
 import ToDo from "./ToDo";
 
+const filterByStatus = ({ todo, displayStatus }) => {
+  if (displayStatus === "all") {
+    return true;
+  } else if (displayStatus === "pending") {
+    return todo.completed === false;
+  } else if (displayStatus === "completed") {
+    return todo.completed === true;
+  } else {
+    return false;
+  }
+};
+
+const filterByImportance = ({ todo, important }) => {
+  if (important === true) {
+    return todo.important === true;
+  } else {
+    return true;
+  }
+};
+
+const filterByText = ({ todo, searchText }) => {
+  if (searchText?.length > 0) {
+    return todo.todoText
+      .toLocaleLowerCase()
+      .includes(searchText.toLocaleLowerCase());
+  } else {
+    return true;
+  }
+};
+
 const ToDoList = ({
   displayStatus,
   toDoList,
@@ -13,33 +43,9 @@ const ToDoList = ({
   return (
     <div className="tasks">
       {toDoList
-        .filter((todo) => {
-          if (displayStatus === "all") {
-            return true;
-          } else if (displayStatus === "pending") {
-            return todo.completed === false;
-          } else if (displayStatus === "completed") {
-            return todo.completed === true;
-          } else {
-            return false; // should not be needed
-          }
-        })
-        .filter((todo) => {
-          if (important === true) {
-            return todo.important === true;
-          } else {
-            return true;
-          }
-        })
-        .filter((todo) => {
-          if (searchText?.length > 0) {
-            return todo.todoText
-              .toLocaleLowerCase()
-              .includes(searchText.toLocaleLowerCase());
-          } else {
-            return true;
-          }
-        })
+        .filter((todo) => filterByStatus({ todo, displayStatus }))
+        .filter((todo) => filterByImportance({ todo, important }))
+        .filter((todo) => filterByText({ todo, searchText }))
         .map((todo) => {
           return (
             <ToDo
